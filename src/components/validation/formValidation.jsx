9002f0027ab30c6c0e27a3bec7652f482ea04c30
Aman404 <amanupadhyay404@gmail.com>
2023-08-16 13:17:50 +0530

    min  and max filed

--- src/components/validation/formValidation.jsx
+++ src/components/validation/formValidation.jsx
@@ -3,9 +3,36 @@ import * as Yup from "yup";
 const generateValidationSchema = (formFields) => {
     const validationSchema = Yup.object().shape(
         formFields.reduce((schema, field) => {
-            schema[field.name] = field.required
-                ? Yup.string().required(`${field.label} is required`)
-                : Yup.string();
+            let fieldSchema;
+
+            switch (field.type) {
+                case "text":
+                    fieldSchema = Yup.string();
+                    break;
+                case "number":
+                    fieldSchema = Yup.number();
+                    break;
+                case "email":
+                    fieldSchema = Yup.string().email(`${field.label} must be a valid email`);
+                    break;
+
+                default:
+                    fieldSchema = Yup.string(); // Default to string if type is not recognized
+            }
+
+            if (field.required) {
+                fieldSchema = fieldSchema.required(`${field.label} is required`);
+            }
+
+            if (field.min !== undefined) {
+                fieldSchema = fieldSchema.min(field.min, `${field.label} must be at least ${field.min}`);
+            }
+
+            if (field.max !== undefined) {
+                fieldSchema = fieldSchema.max(field.max, `${field.label} must be at most ${field.max}`);
+            }
+
+            schema[field.name] = fieldSchema;
             return schema;
         }, {})
     );
--- src/config/formConfig.js
+++ src/config/formConfig.js
@@ -1,57 +1,68 @@
-const formConfig ={
-    "forms": [
-      {
-        "id": "form1",
-        "fields": [
-          {
-            "name": "firstName",
-            "label": "First Name",
-            "type": "text",
-            "required": true
-          },
-          {
-            "name": "lastName",
-            "label": "Last Name",
-            "type": "text",
-            "required": true
-          }
-        ]
-      },
-      {
-        "id": "form2",
-        "fields": [
-          {
-            "name": "email",
-            "label": "Email",
-            "type": "email",
-            "required": true
-          },
-          {
-            "name": "phoneNumber",
-            "label": "Phone Number",
-            "type": "tel",
-            "required": false
-          }
-        ]
-      },
-      {
-        "id": "form3",
-        "fields": [
-          {
-            "name": "phone",
-            "label": "phone",
-            "type": "number",
-            "required": true
-          },
-          {
-            "name": "lastName",
-            "label": "Last Name",
-            "type": "text",
-            "required": true
-          }
-        ]
-      }
-    ]
-  }
+const formConfig = {
+  "forms": [
+    {
+      "id": "form1",
+      "fields": [
+        {
+          "name": "firstName",
+          "label": "First Name",
+          "type": "text",
+          "required": true,
+          "min": 2, 
+          "max": 15, 
+        },
+        {
+          "name": "lastName",
+          "label": "Last Name",
+          "type": "text",
+          "required": true,
+          "min": 2, 
+          "max": 15,
+        }
+      ]
+    },
+    {
+      "id": "form2",
+      "fields": [
+        {
+          "name": "email",
+          "label": "Email",
+          "type": "email",
+          "required": true,
+          "min": 2, 
+          "max": 15, 
+        },
+        {
+          "name": "phoneNumber",
+          "label": "Phone Number",
+          "type": "number",
+          "required": false,
+          "min":1, 
+          "max": 10, 
+        }
+      ]
+    },
+    {
+      "id": "form3",
+      "fields": [
+        {
+          "name": "phone",
+          "label": "phone",
+          "type": "number",
+          "required": true,
+          "min":1, 
+          "max": 10,
+        },
+        {
+          "name": "lastName",
+          "label": "Last Name",
+          "type": "text",
+          "required": true,
+          "min":2, 
+          "max": 10,
+        }
+      ]
+    }
+  ]
+}
 export default formConfig;
-  
