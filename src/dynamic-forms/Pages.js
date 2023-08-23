0f3f32b3ff436b8cf69ddc410b64c71e3bb739f2
Aman404 <amanupadhyay404@gmail.com>
2023-08-23 17:37:37 +0530

    ...

--- src/dynamic-forms/Pages.js
+++ src/dynamic-forms/Pages.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Sections from "./Sections";
 
 const Pages = ({ pData }) => {
   const data = pData;
   const sData = data[0].sections;
+  const s_0Data = data[0].sections_0;
+  const [activeSectionIndex, setActiveSectionIndex] = useState(0);
+
+  const handleNextSection = () => {
+    setActiveSectionIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const handlePreviousSection = () => {
+    setActiveSectionIndex((prevIndex) => prevIndex - 1);
+  };
+
   return (
     <div>
-      <h2>Pages From</h2>
+      <h2>Pages Form</h2>
       {data.map((d) => (
-        <div>
+        <div key={d.id}>
           <p>{d.serial}</p>
           <p>{d.name}</p>
           <p>{d.heading}</p>
-          <Sections sData={sData} />
+          <Sections sData={sData} activeSectionIndex={activeSectionIndex} />
+          <Sections sData={s_0Data} />
+          {activeSectionIndex > 0 && (
+            <button onClick={handlePreviousSection}>Back</button>
+          )}
+          {activeSectionIndex < sData.length - 1 && (
+            <button onClick={handleNextSection}>Next</button>
+          )}
         </div>
       ))}
     </div>
--- src/dynamic-forms/Sections.js
+++ src/dynamic-forms/Sections.js
@@ -1,10 +1,29 @@
 import React from "react";
-
 import Components from "./Components";
 
+<<<<<<< HEAD
+const Sections = ({ sData, activeSectionIndex }) => {
+  const section = sData[activeSectionIndex];
+  const cData = section && section.components ? section.components : [];
+
+  return (
+    <div>
+      <h2>Sections Data</h2>
+      <div>
+        {section && (
+          <>
+            <p>{section.serial}</p>
+            <h4>{section.name}</h4>
+            <p>{section.heading}</p>
+            <Components cData={cData} />
+          </>
+        )}
+      </div>
+=======
 const Sections = ({ sData }) => {
   const data = sData;
   const cData = data[0].components;
+  const c2Data = data[0].components2;
   console.log("Section Data ", data);
   return (
     <div>
@@ -15,8 +34,10 @@ const Sections = ({ sData }) => {
           <p>{d.name}</p>
           <p>{d.heading}</p>
           <Components cData={cData} />
+          <Components cData={c2Data} />
         </div>
       ))}
+>>>>>>> 0612fee3430b9085ee60f4afb77cb9f427a1ca73
     </div>
   );
 };
