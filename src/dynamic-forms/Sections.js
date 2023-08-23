41ae305e64a1527546e5ad5ebb454290c7096931
Aman404 <amanupadhyay404@gmail.com>
2023-08-23 17:37:46 +0530

    .

--- src/dynamic-forms/Sections.js
+++ src/dynamic-forms/Sections.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Components from "./Components";
 
-<<<<<<< HEAD
 const Sections = ({ sData, activeSectionIndex }) => {
   const section = sData[activeSectionIndex];
   const cData = section && section.components ? section.components : [];
@@ -13,31 +12,12 @@ const Sections = ({ sData, activeSectionIndex }) => {
         {section && (
           <>
             <p>{section.serial}</p>
-            <h4>{section.name}</h4>
+            <p>{section.name}</p>
             <p>{section.heading}</p>
             <Components cData={cData} />
           </>
         )}
       </div>
-=======
-const Sections = ({ sData }) => {
-  const data = sData;
-  const cData = data[0].components;
-  const c2Data = data[0].components2;
-  console.log("Section Data ", data);
-  return (
-    <div>
-      <h2>Sections Data</h2>
-      {data.map((d) => (
-        <div>
-          <p>{d.serial}</p>
-          <p>{d.name}</p>
-          <p>{d.heading}</p>
-          <Components cData={cData} />
-          <Components cData={c2Data} />
-        </div>
-      ))}
->>>>>>> 0612fee3430b9085ee60f4afb77cb9f427a1ca73
     </div>
   );
 };
