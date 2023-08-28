79868139ac21a0fa49bc837c986c066b5143b70e
Aman404 <amanupadhyay404@gmail.com>
2023-08-28 17:05:44 +0530

    ..

---
+++ src/dynamic-forms/Sections.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Components from "./Components";
+
+const Sections = ({ sData, activeSectionIndex }) => {
+  const section = sData[activeSectionIndex];
+  const cData = section && section.components ? section.components : [];
+
+  return (
+    <div>
+      <h2>Sections Data</h2>
+      <div>
+        {section && (
+          <>
+            {/* <p>{section.serial}</p> */}
+            <h3>{section.name}</h3>
+            <p>{section.heading}</p>
+            <Components cData={cData} />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Sections;
